e7513942c07a7733d53b1f545152aa43f037602c
Vijaya Panga <VijayalakshmiPanga@gmail.com>
2019-01-07 16:12:45 +0000

    whileloop_example

---
+++ src/javaLearning/WhileLoopPrint.java
@@ -0,0 +1,14 @@
+package javaLearning;
+
+public class WhileLoopPrint {
+    public static void main(String[] args) {
+        int count =1;
+
+        while(count<=100)
+        {
+            System.out.println(count);
+            count++;
+        }
+
+    }
+}
